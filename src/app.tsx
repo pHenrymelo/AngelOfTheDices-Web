c88229a220477cbc2204196c11af358a920dd581
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-09-29 11:00:08 -0300

    feat: setup app base

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from './components/theme/theme-provider';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div></div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from 'react-router';
+import { NotFound } from './pages/404';
+import { Dashboard } from './pages/app/dashboard/dashboard';
+import { SignIn } from './pages/auth/sign-in';
+import { SignUp } from './pages/auth/sign-up';
+import { AppLayout } from './pages/layouts/app';
+import { AuthLayout } from './pages/layouts/auth';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <NotFound />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
+  },
+]);
