d378f487513175d6a26a292a1f25388de2d48155
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-09-29 15:27:11 -0300

    feat: add header pages shortcut

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,5 @@
-import { Separator } from '@radix-ui/react-separator';
-import { Home, LucideListChecks } from 'lucide-react';
 import Logo from '@/assets/logo-violet.png';
+import { Dices, FileSpreadsheet, Home } from 'lucide-react';
 import { NavLink } from './nav-link';
 import { ThemeToggle } from './theme/theme-togle';
 
@@ -8,18 +7,21 @@ export function Header() {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center gap-6 px-6">
-        <div className="w-12 h-12">
+        <div className="w-12 h-12 ">
           <img src={Logo} alt="a dice with wings" />
         </div>
-        <Separator orientation="vertical" className="h-6" />
-        <nav className="flex items-center space-x-4 lg:space-x-6">
+        <nav className="flex items-center space-x-4 lg:space-x-6 border-l h-full px-4">
           <NavLink to="/">
             <Home className="h-4 w-4" />
             Home
           </NavLink>
           <NavLink to="/data">
-            <LucideListChecks className="h-4 w-4" />
-            Quests
+            <FileSpreadsheet className="h-4 w-4" />
+            Fichas
+          </NavLink>
+          <NavLink to="/dices">
+            <Dices className="h-4 w-4" />
+            Dados
           </NavLink>
         </nav>
         <div className="flex ml-auto items-center gap-2">
