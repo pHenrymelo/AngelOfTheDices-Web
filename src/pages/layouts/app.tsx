280ed1343382c4e075f5f0a816f0cb80722bf916
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-09-30 16:30:42 -0300

    fix: fix header style bug

--- src/pages/layouts/app.tsx
+++ src/pages/layouts/app.tsx
@@ -3,9 +3,9 @@ import { Header } from '@/components/header';
 
 export function AppLayout() {
   return (
-    <div className="flex flex-col min-h-screen antialiased">
+    <div className="flex flex-col h-screen antialiased pt-16">
       <Header />
-      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+      <div className="flex flex-1 flex-col gap-4 overflow-y-auto p-8 pt-6">
         <Outlet />
       </div>
     </div>
