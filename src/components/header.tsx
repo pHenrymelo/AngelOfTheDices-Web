1ab3ed303763714f0743c033828133f60adce64b
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-10-14 17:58:58 -0300

    responsivity(style): implements header responsivity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,12 @@ import { ThemeToggle } from './theme/theme-togle';
 export function Header() {
   return (
     <div className="fixed top-0 left-0 z-50 w-full border-b bg-primary/5 backdrop-blur-xl">
-      <div className="flex h-16 items-center gap-3 px-3 md:gap-6 md:mx-6">
-        <img className="size-8" src={Logo} alt="a dice with wings" />
+      <div className="flex h-16 items-center gap-3 px-3 md:gap-6 md:px-6">
+        <img
+          className="size-10 md:size-14"
+          src={Logo}
+          alt="a dice with wings"
+        />
         <nav className="flex items-center space-x-4 lg:space-x-6 border-l h-full px-4">
           <NavLink to="/">
             <FileSpreadsheet className="h-4 w-4" />
